970de8bd4cb255092449155a7a1ed86c0a1ea12e
Alejo2412qwe <111301530+Alejo2412qwe@users.noreply.github.com>
2023-02-09 12:14:19 -0500

    Add files via upload

---
+++ ProyectoM4A/src/main/java/com/ista/springboot/app/ProyectoM4AApplication.java
@@ -0,0 +1,13 @@
+package com.ista.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoM4AApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoM4AApplication.class, args);
+	}
+
+}
